19545da09d3f32eb403c491d145217983d2d48d3
Capucine Cusinberche <capucine.cusinberche@gmail.com>
2018-10-19 00:46:10 +0200

    added geocoding migration to DB

---
+++ db/migrate/20181018224110_add_coordinates_to_cities.rb
@@ -0,0 +1,10 @@
+class AddCoordinatesToCities < ActiveRecord::Migration[5.1]
+  def change
+    add_column :cities, :coordinates, :string
+  end
+end
+
+
+
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181010153121) do
+ActiveRecord::Schema.define(version: 20181018224110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20181010153121) do
     t.string "geocoding"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "coordinates"
   end
 
   create_table "provinces", force: :cascade do |t|
