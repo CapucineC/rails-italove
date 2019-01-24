1502b7619b570ef82ee511e0fd54c996fd616499
Capucine Cusinberche <capucine.cusinberche@gmail.com>
2019-01-24 19:56:01 +0100

    nested url fixed

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,14 +1,10 @@
 class CitiesController < ApplicationController
   def show
-    @province = Province.find(params[:id])
-    @city = City.find(params[:province_id])
+    @province = Province.find(params[:province_id])
+    @city = City.find(params[:id])
   end
 
-
-# methode dans le controller
-# def gallery
-# @city = City.find(params[:id])
-# city.picture.each do |picture| %>
-# end
-# end
+  def to_param
+    [id, name.parameterize].join("-")
+  end
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,3 +1,7 @@
 class Province < ApplicationRecord
   has_many :cities
+
+  def to_param
+    [id, name.parameterize].join("-")
+  end
 end
