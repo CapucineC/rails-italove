34182557e6877b4580c02d69fc2c3da1dad0ba42
Capucine Cusinberche <capucine.cusinberche@gmail.com>
2020-08-07 10:36:52 +0200

    lb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4786,13 +4786,13 @@ Picture.create(
   city: bari
 )
 Picture.create(
-  url: "https://raw.githubusercontent.com/CapucineC/iMages/master/Bari/ba_22.jpg",
+  url: "https://raw.githubusercontent.com/CapucineC/iMages/master/Bari/ba_21.jpg",
   position: 21,
   css_class:"portrait",
   city: bari
 )
 Picture.create(
-  url: "https://raw.githubusercontent.com/CapucineC/iMages/master/Bari/ba_21.jpg",
+  url: "https://raw.githubusercontent.com/CapucineC/iMages/master/Bari/ba_22.jpg",
   position: 22,
   city: bari
 )
@@ -4843,36 +4843,24 @@ Picture.create(
 Picture.create(
   url: "https://raw.githubusercontent.com/CapucineC/iMages/master/Bari/ba_31.jpg",
   position: 31,
-  css_class:"portrait",
+  css_class:"landscape",
   city: bari
 )
 Picture.create(
   url: "https://raw.githubusercontent.com/CapucineC/iMages/master/Bari/ba_33.jpg",
   position: 32,
-  css_class:"zoom",
+  css_class:"landscape",
   city: bari
 )
 Picture.create(
   url: "https://raw.githubusercontent.com/CapucineC/iMages/master/Bari/ba_32.jpg",
   position: 33,
-  css_class:"zoom",
+  css_class:"landscape",
   city: bari
 )
 Picture.create(
   url: "https://raw.githubusercontent.com/CapucineC/iMages/master/Bari/ba_34.jpg",
   position: 34,
-  css_class:"portrait",
-  city: bari
-)
-Picture.create(
-  url: "https://raw.githubusercontent.com/CapucineC/iMages/master/Bari/ba_35.jpg",
-  position: 35,
-  css_class:"landscape",
-  city: bari
-)
-Picture.create(
-  url: "https://raw.githubusercontent.com/CapucineC/iMages/master/Bari/ba_36.jpg",
-  position: 36,
   css_class:"landscape",
   city: bari
 )
